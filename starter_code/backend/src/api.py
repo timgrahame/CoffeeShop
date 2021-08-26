5890b8ec0f2543fb704890670e24dddb85593dac
timgrahame <83875027+timgrahame@users.noreply.github.com>
2021-08-26 10:00:01 +0100

    first upload

---
+++ starter_code/backend/src/api.py
@@ -0,0 +1,199 @@
+import os
+from types import MethodDescriptorType
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+from flask_cors import CORS
+
+from .database.models import db_drop_and_create_all, setup_db, Drink
+from .auth.auth import AuthError, requires_auth
+
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+'''
+@TODO uncomment the following line to initialize the datbase
+!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
+!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
+!! Running this funciton will add one
+'''
+db_drop_and_create_all()
+
+
+# ROUTES
+'''
+@TODO implement endpoint
+    GET /drinks
+        it should be a public endpoint
+        it should contain only the drink.short() data representation
+    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        or appropriate status code indicating reason for failure
+'''
+@app.route('/drinks')
+def get_drinks():
+    try:
+        drinks = Drink.query.all()
+        return jsonify({
+        'success': True,
+        'drinks': [drink.short() for drink in drinks],
+        }), 200
+    
+    except:
+        abort(404)
+
+
+'''
+@TODO implement endpoint
+    GET /drinks-detail
+        it should require the 'get:drinks-detail' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        or appropriate status code indicating reason for failure
+'''
+
+@app.route('/drinks_detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
+    try:
+        drinks = Drink.query.all()
+        return jsonify({
+        'success': True,
+        'drinks': [drink.long() for drink in drinks],
+        }), 200
+    
+    except:
+        abort(405)
+
+'''
+@TODO implement endpoint
+    POST /drinks
+        it should create a new row in the drinks table
+        it should require the 'post:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+        or appropriate status code indicating reason for failure
+'''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def post_drinks(payload):
+    try:
+        body = request.get_json()
+        if body is None:
+            abort(400)
+        
+        drink_title = body.get("title", None)
+        drink_recipe = body.get("recipe", None)
+
+        create_drink = Drink(title=drink_title, recipe=json.dumps(drink_recipe))
+        create_drink.insert()
+
+        return jsonify({
+        'success': True,
+        'drinks': [create_drink.long()],
+        }), 200
+
+#    except:
+#        abort(400)
+    except Exception as e:
+        print(e)
+        abort(400)
+
+'''
+@TODO implement endpoint
+    PATCH /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should update the corresponding row for <id>
+        it should require the 'patch:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+        or appropriate status code indicating reason for failure
+'''
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drinks(payload, drink_id):
+        try:
+            body = request.get_json()
+            drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+            if body is None:
+                abort(404)#
+
+            title = body.get("title", None)
+            recipe = body.get("recipe", None)
+
+            drink.title = title
+            drink.recipe = json.dumps(recipe)
+            drink.update()
+
+            return jsonify({
+            'success': True,
+            'drinks': [drink.long()],
+            }), 200
+
+        except:
+            abort(400)
+'''
+@TODO implement endpoint
+    DELETE /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should delete the corresponding row for <id>
+        it should require the 'delete:drinks' permission
+    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+        or appropriate status code indicating reason for failure
+'''
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drinks(drink_id):
+        try:
+            body = request.get_json()
+            drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+            if body is None:
+                abort(404)#
+            drink.delete()
+
+            return jsonify({
+            'success': True,
+            'drinks': [drink.long()],
+            }), 200
+
+        except:
+            abort(400)
+
+# Error Handling
+'''
+Example error handling for unprocessable entity
+'''
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+
+'''
+@TODO implement error handlers using the @app.errorhandler(error) decorator
+    each error handler should return (with approprate messages):
+             jsonify({
+                    "success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
+
+'''
+
+'''
+@TODO implement error handler for 404
+    error handler should conform to general task above
+'''
+
+
+'''
+@TODO implement error handler for AuthError
+    error handler should conform to general task above
+'''
